f563f0b3fcebf7d1bb62c3bbacdeb4a77e7932cd
José A. González <champuness@gmail.com>
2021-02-20 16:37:22 +0100

    Implement a Styled Component
    - Adding styled-components to the project
    - Create some components for testing the library

--- src/app/home/HomeComponent.jsx
+++ src/app/home/HomeComponent.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import { PropTypes } from 'prop-types';
 
-const HomeComponent = ({ browser }) => <div>{`Es un navegador? ${browser}`}</div>;
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`;
+
+const HomeComponent = ({ browser }) => (
+    <Wrapper>
+        <Title>{`Es navegador o no? ${browser}`}</Title>
+    </Wrapper>
+);
 
 HomeComponent.propTypes = {
     browser: PropTypes.bool
